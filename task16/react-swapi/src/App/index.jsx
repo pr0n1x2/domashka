b3f38134ac1de2e7f54e638e2fb10f0353bc636a
Vladimir Skirga <vladimir.skirga84@gmail.com>
2019-06-21 11:51:36 +0300

    React swapi

---
+++ task16/react-swapi/src/App/index.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Header from '../Header';
+import axios from 'axios';
+import styles from './styles.module.scss';
+
+class App extends Component {
+  state = {
+    films: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(`https://swapi.co/api/films/`);
+    const films = [];
+
+    for (let film of response.data.results) {
+      films.push(film.title);
+    }
+
+    this.setState({ films: films });
+  }
+
+  render() {
+    const films = this.state.films;
+
+    return (
+      <div className={styles.App}>
+        <Header films={films} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ task16/react-swapi/src/Header/index.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import logo from '../Header/star-wars.svg';
+import styles from './styles.module.scss';
+
+class Header extends Component {
+    render() {
+        const films = this.props.films;
+
+        const movies = films.map((title, index) => {
+            return <li key={index}>{title}</li>
+        });
+
+        return (
+            <header className={styles['App-header']}>
+                <img src={logo} className={styles['App-logo']} alt="logo"/>
+                <p className={styles['App-title']}>
+                    List of all movies from the Star Wars universe
+                </p>
+                <ul className={styles['App-movies']}>{movies}</ul>
+            </header>
+        );
+    }
+}
+
+export default Header;
